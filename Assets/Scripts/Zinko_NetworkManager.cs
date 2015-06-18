5c3d810c7264e14551a5f27a6e9e974deefdf98c
Zinko <znmarteen21@gmail.com>
2015-06-18 17:13:41 +0200

    Started rewriting network manager

---
+++ Assets/Scripts/Zinko_NetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zinko_NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
