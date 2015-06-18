57b933293a28d7a534dff1b9e99d8728ba9b9ce0
Zinko <znmarteen21@gmail.com>
2015-06-18 20:15:39 +0200

    Changeable Player Prefabs
    Jakab

---
+++ Assets/Scripts/Player_PreviewCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_PreviewCharacter : MonoBehaviour {
+    private int selected;
+    [SerializeField]
+    private GameObject myNetworkManager;
+    private Zinko_NetworkManager myZinko_NetworkManager;
+	// Use this for initialization
+	void Start () {
+        myZinko_NetworkManager = myNetworkManager.GetComponent<Zinko_NetworkManager>();
+	}
+    public void OnClick() {
+        myZinko_NetworkManager.NextPlayerPrefab();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Zinko_NetworkManager.cs
+++ Assets/Scripts/Zinko_NetworkManager.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Zinko_NetworkManager : MonoBehaviour {
+public class Zinko_NetworkManager : NetworkManager {
+    [SerializeField]
+    private GameObject[] Zinko_PrefabOptions;
+    private int Zinko_Selected = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void NextPlayerPrefab(){
+        if (Zinko_PrefabOptions.Length == 0) {
+            Debug.LogError("No prefab options available");
+            return;
+        }
+        if (Zinko_Selected + 1 == Zinko_PrefabOptions.Length) {
+            Zinko_Selected = 0;
+        }
+        else {
+            Zinko_Selected++;
+        }
+        this.playerPrefab = Zinko_PrefabOptions[Zinko_Selected];
+        return;
+    }
 }
